12e17be703ab0b7619ec8f1b69e7dc6b073ce0ca
rocky <rocky-gow@hotmail.com>
2018-09-03 12:21:29 +0200

    Joey pusht code

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -10,19 +10,24 @@ public class HalloRocDev {
     public static void main(String[] args) {
         System.out.println("Hallo Rocky!");
     }
-}
 
     public void paint(Graphics g) {
         setBackground(Color.black);
         g.setColor(Color.yellow);
         g.drawRect(50, 120, 270, 120);
         g.fillrect(20, 130, 100, 50);
-    }
-}
-int x[] = { 20,20,130,100,50};
+        int x[] = { 20,20,130,100,50};
         int y[] = { , 110, 160, 190, 100 };
         g.drawPolygon (x, y, 3);
 
         g.fillPolygon (x1, y1, 3);
-<applet code="DrawingPolygons.class" width="350" height="300">
+
+
+    }
+
+
+
+
+
+        
 
