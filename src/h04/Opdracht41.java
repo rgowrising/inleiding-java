529263cfd0e6d202fc5af080a265e96737720904
rocky <rocky-gow@hotmail.com>
2018-09-07 14:28:05 +0200

    praktijkopdracht

--- src/h04/Opdracht41.java
+++ src/h04/Opdracht41.java
@@ -29,8 +29,7 @@ public class Opdracht41 extends Applet {
         g.drawString("100", 70, 350 );
         // Staaf van Valerie
         g.setColor(Color.BLACK);
-        g.drawRect(100, 500, 100, 100);
-        g.fillArc(20, 20, 100, 50, 90, 45);
+        g.fillRect(100, 500, 100, 100);
         g.setColor(Color.yellow);
         // Staaf van Jeroen
         g.drawRect(200, 350, 100, 250);
---
+++ src/h04/Praktijkopdracht.java
@@ -0,0 +1,25 @@
+package h04;
+
+import java.awt.*;
+
+public class Praktijkopdracht {
+    //een (lege) methode die de Applet initialiseert
+    public void init() {
+
+    }
+
+    //een methode die de inhoud van het scherm tekent
+    public void paint(Graphics g) {
+
+        g.drawLine(20,20,50,50);
+        g.drawRect(50,150,100,100);
+        g.drawOval(60,60,50,50);
+
+
+
+
+    }
+
+
+
+}
