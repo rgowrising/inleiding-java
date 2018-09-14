fcc75cf0e3c5f62b14e11eb04be89a37ce6fa48b
rocky <rocky-gow@hotmail.com>
2018-09-14 14:10:34 +0200

    Opdracht71

---
+++ src/h07/Opdracht71.java
@@ -0,0 +1,13 @@
+package h07;
+import java.awt.*;
+import java.applet.*;
+public class Opdracht71 extends Applet {
+
+    public void init() {
+        setSize(1024,768);
+    }public void paint(Graphics g) {
+        g.drawString("Vraag2 = component",20,30);
+        g.drawString("Vraag3 = eerste is oud en de tweede is nieuw",20,60);
+
+    }
+}
