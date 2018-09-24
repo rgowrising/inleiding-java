e6a1d2010a6b1889d649070b681f2fd2d0e2e42a
rocky <rocky-gow@hotmail.com>
2018-09-24 11:25:12 +0200

    Opdracht101

---
+++ src/h010/Opdracht101.java
@@ -0,0 +1,38 @@
+package h010;
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+public class Opdracht101 extends Applet {
+    TextField tekstvak;
+    Label label;
+    String s, tekst;
+    int inputgetal;
+    int maxgetal;
+
+    public void init() {
+        tekstvak = new TextField("", 20);
+        label = new Label("Type een getal en druk op enter");
+        tekstvak.addActionListener( new TekstvakListener() );
+        tekst = "";
+        add(label);
+        add(tekstvak);
+        maxgetal = 0;
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("" + maxgetal, 50, 60 );
+    }
+    class TekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            s = tekstvak.getText();
+            inputgetal = Integer.parseInt( s);
+
+            if ( inputgetal > maxgetal) {
+               maxgetal = inputgetal;
+            }
+            repaint();
+        }
+
+
+    }
+}
