2a1af341d49399233c9608c8c9d75bbe9ec8bc92
rocky <rocky-gow@hotmail.com>
2018-09-27 11:10:39 +0200

    Opdracht112

--- src/h011/Opdracht112.java
+++ src/h011/Opdracht112.java
@@ -1,20 +1,20 @@
 package h011;
 import java.awt.*;
 import java.applet.*;
-public class Opdracht111 extends Applet {
+public class Opdracht112 extends Applet {
 
     public void init() {
     }
 
     public void paint(Graphics g) {
-        int teller;
+        int teller = 10;
         int y = 0;
-        for (teller = 0; teller < 10; teller++) {
-            y += 20;
-            g.drawLine(50 , y, 300, y );
-            g.drawString("" + teller, 305, y );
 
-            }
+        while(teller < 21) {
+            y += 20;
+            g.drawString("" + teller, 305, y );
+            teller++;
         }
     }
+        }
 
