3afa209f3c62ed7a5b998378adab2e00da86df86
rocky <rocky-gow@hotmail.com>
2018-10-01 11:34:05 +0200

    Opdracht115

---
+++ src/h011/Opdracht115.java
@@ -0,0 +1,21 @@
+package h011;
+import java.awt.*;
+import java.applet.*;
+public class Opdracht115 extends Applet {
+
+
+    public void init() {
+
+    }
+    public void paint(Graphics g) {
+        g.drawRoundRect(20, 20, 35, 35, 10, 10);
+        g.drawRoundRect(45, 35, 35, 35, 10, 10);
+        g.drawRoundRect(70, 55, 35, 35, 10, 10);
+        g.drawRoundRect(90, 70, 35, 35, 10, 10);
+        g.drawRoundRect(115, 90, 35, 35, 10, 10);
+        }
+    }
+
+
+
+
