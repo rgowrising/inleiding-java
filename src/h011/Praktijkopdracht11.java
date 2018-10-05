5100c16082a65661c1fd954e604c44f7ae4b58af
rocky <rocky-gow@hotmail.com>
2018-10-05 14:08:32 +0200

    Praktijkopdracht11

---
+++ src/h011/Praktijkopdracht11.java
@@ -0,0 +1,21 @@
+package h011;
+import java.awt.*;
+import java.applet.*;
+public class Praktijkopdracht11 extends Applet {
+
+    public void init () {
+
+    }
+    public void paint(Graphics g) {
+        g.drawString("1*2=2",50,60);
+        g.drawString("2*2=4",50,80);
+        g.drawString("3*2=6",50,100);
+        g.drawString("4*2=8",50,120);
+        g.drawString("5*2=10",50,140);
+        g.drawString("6*2=12",50,160);
+        g.drawString("7*2=14",50,180);
+        g.drawString("8*2=16",50,200);
+        g.drawString("9*2=18",50,220);
+        g.drawString("10*2=20",50,240);
+    }
+}
